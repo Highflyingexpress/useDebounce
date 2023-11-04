e3172c1455b72c700f446608b9bec4386d572539
AntonChe <tonybetony@yandex.ru>
2023-11-05 01:13:47 +0300

    useTransition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// startTransition для классовых
+import { useEffect, useState, useTransition } from "react";
+import "./App.css";
+
+const URL = "https://jsonplaceholder.typicode.com";
+
+const filterBySearch = (entities, search) => {
+  return entities.filter((item) =>
+    item.name.concat(item.body).includes(search.trim())
+  );
+};
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+  const [comments, setComments] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // fetch comments from jsonplaceholder
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`${URL}/comments`);
+      const data = await response.json();
+      setComments(data);
+    };
+    fetchPosts();
+  }, []);
+
+  // handle input typing
+  const handleSearch = (e) => {
+    startTransition(() => {
+      // all not-urgent updates here
+      setSearch(e.target.value);
+    });
+    // позволяет разбить операцию на чанки не блокируя общий пользовательский поток
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Search comments by useTransition</h2>
+      <input className="searchInput" onChange={handleSearch} />
+      <>{isPending && <h2>Loading...</h2>}</>
+      <h2>searched comments:</h2>
+      <ul className="comment">
+        {filterBySearch(comments, search)?.map(({ id, name, body }) => (
+          <li key={id}>
+            {id}
+            <h3>{name} </h3>
+            <p>{body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
